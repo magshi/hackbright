fcdd3f5bc38a82f9ddcdac5a37d828cf9e43e1ff
Maggie Shine <magksh@gmail.com>
2014-06-06 16:17:56 -0700

    Starting Exercise 05

--- Homework/Homework-03/accounting.py
+++ Homework/Homework-03/accounting.py
@@ -1,6 +1,8 @@
 def parse_sales_log(filename):
     sales_log = open(filename)
 
+    delinquents = []
+
     for line in sales_log:
         entries = line.split(',')
 
@@ -14,10 +16,14 @@ def parse_sales_log(filename):
         if customer_owes != customer_paid:
             print "%s (#%s) ordered %d melons and has paid $%r; customer owes $%.2f. \n" %(customer_name, customer_id, melons_ordered, customer_paid, customer_expected)
 
+            delinquents.append(customer_name)
+
+    print delinquents
+
 def main():
-    print "Enter log file: "
-    filename = raw_input()
-    parse_sales_log(filename)
+    # print "Enter log file: "
+    # filename = raw_input()
+    parse_sales_log("customer_orders.csv")
 
 if __name__ == "__main__":
     main()
