88e0d5242d84e2fbc87ac3a1575f60f157e10942
Maggie Shine <magksh@gmail.com>
2014-06-13 15:12:05 -0700

    Reconstructing loop to account for length and periods

--- Exercises/Exercise-08/markov.py
+++ Exercises/Exercise-08/markov.py
@@ -36,6 +36,7 @@ def make_chains(corpus1, corpus2):
 
     return [dict1, dict2]
 
+# this function runs until the first tuple chosen begins with an uppercase letter, then it passes that tuple as the first key to the make_text function
 def check_upper(chains, switch):
     random_tuple = random.choice(chains[switch].keys())
 
@@ -49,15 +50,18 @@ def make_text(chains):
     based off an original text."""
     switch = 0
 
+    # start with a tuple that begins with an uppercase letter
     random_tuple = check_upper(chains, switch)
 
     # random_output is a random tuple from one dictionary plus a random tuple from the other dictionary
     random_output = random_tuple[0] + ' ' + random_tuple[1]
+    # sets current key to the first tuple
     current_key = random_tuple
 
     i = 0
     # i = however many times you want the script to run. depends on how long you want your output to be
-    while i < 20:
+    # 23 seems to be a good number for Twitter-ready output (<140 chars)
+    while i < 23:
         i += 1
         if chains[(switch + 1) % 2].get(current_key):
             # flip the switch so script will access the other dictionary
@@ -70,15 +74,26 @@ def make_text(chains):
             current_key = (current_key[1], next_word)
             random_output += " %s" % next_word
 
-    index_of_period = 0
-    if len(random_output) > 100 and len(random_output) < 140:
+    print len(random_output)
+    print random_output
+
+    new_string = ''
+
+    # if the string is a good length for Twitter and has a period
+    if len(random_output) > 110 and len(random_output) < 140 and '.' in random_output:
+        index_of_period = 0
         for index, value in enumerate(random_output):
             if value == '.':
                 index_of_period = index
 
-    new_string = random_output[:index_of_period] + '.'
+        new_string = random_output[:index_of_period] + '.'
+    # otherwise, add another tuple and then add a period
+    else:
+        print False
+        next_word = random.choice(chains[switch][current_key])
+        random_output 
+
     return new_string
-    # return random_output[::-1]
 
 def main():
     # runs the markov.py script and passes two input files as arguments
