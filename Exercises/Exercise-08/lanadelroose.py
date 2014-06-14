f746a5e3cbaab85e5577664fd238abd755dea3a6
Maggie Shine <magksh@gmail.com>
2014-06-13 17:31:55 -0700

    set up @lanadelroose

---
+++ Exercises/Exercise-08/lanadelroose.py
@@ -0,0 +1,16 @@
+import twitter
+
+api_file = open("lanakeys.txt").read()
+api_keys = api_file.split()
+api_dict = {}
+
+for item in api_keys:
+	key_value = item.split(':')
+	api_dict[key_value[0]] = key_value[1]
+
+api = twitter.Api(consumer_key = api_dict['consumer_key'],
+	consumer_secret = api_dict['consumer_secret'],
+	access_token_key = api_dict['access_token_key'],
+	access_token_secret = api_dict['access_token_secret'])
+
+print api.VerifyCredentials()
