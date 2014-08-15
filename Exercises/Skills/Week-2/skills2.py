b94f0a9433806a5c73fd8eeb74a3669fdbdc6acc
Maggie Shine <magksh@gmail.com>
2014-08-14 19:15:02 -0700

    updating to include whiteboarding exercises et al

--- Exercises/Skills/Week-2/skills2.py
+++ Exercises/Skills/Week-2/skills2.py
@@ -18,27 +18,14 @@ def count_unique(string1):
 		i += 1
 	print string_dict
 
-count_unique(string1)
+# count_unique(string1)
 
 """
 Given two lists, (without using the keyword 'in' or the method 'index')
 return a list of all common items shared between both lists
 """
 def common_items(list1, list2):
-	pass
-
-#     new_list = []
-#     i = 0
-
-#     if list1[i] == list2[i]
-
-#     if list1[i] == list2[i]:
-#         new_list += list1[i]
-#         i += 1
-#     else:
-#         i += 1
-
-#     print new_list
+	return list(set(list1).intersection(list2))
 
 common_items(list1, list2)
 
@@ -48,16 +35,23 @@ return a list of all common items shared between both lists. This time,
 use a dictionary as part of your solution.
 """
 def common_items2(list1, list2):
-	dict1 = {}
-	dict2 = {}
+	duplicate_dict = {}
 
-	for item in list1:
-		dict1[item] = item
-	print dict1
+	for value in list1:
+		if duplicate_dict.get(value):
+			duplicate_dict[value] = 1
+		else:
+			duplicate_dict[value] += 1
+	for value in list2:
+		if duplicate_dict.get(value):
+			duplicate_dict[value] += 1
+		else:
+			duplicate_dict[value] = 1
 
-	for item in list2:
-		dict2[item] = item
-	print dict2
+	for key, value in duplicate_dict.iteritems():
+		if len(value) > 1:
+			print "The number '%d' appears %d times.\n" % (key, value)
+	
 
 # common_items2(list1, list2)
 
@@ -65,7 +59,12 @@ def common_items2(list1, list2):
 Given a list of numbers, return list of number pairs that sum to zero
 """
 def sum_zero(list1):
-	print list1
+	dictionary = {}
+	for number in list1:
+		dictionary[number] = number
+	for number in list1:
+		if dictionary.get(number*-1):
+			print dictionary[number], number
 
 # sum_zero(list1)
 
@@ -73,7 +72,12 @@ def sum_zero(list1):
 Given a list of words, return a list of words with duplicates removed
 """
 def find_duplicates(words):
-	pass
+	dictionary = {}
+	for word in words:
+		dictionary[word] = word
+	for word in words:
+		if dictionary.get(word):
+			print "The word %s is a duplicate with %s" % (word, dictionary.get(word))
 
 # find_duplicates(words)
 
@@ -83,15 +87,18 @@ Bonus: do it on a file instead of the list provided
 Bonus: print the words in alphabetical order in ascending order of length
 """
 def word_length(words):
-    word_count = {}
-    alpha_list = []
+    word_length_dict = {}
+    list_of_lengths = []
     for word in words:
-      if word in word_count:
-        word_count[word] += 1
-      else:
-        word_count[word] = 1
-    for key, value in word_count.iteritems():
-      print key, value
+    	if len(word) in word_length_dict:
+    		word_length_dict[len(word)].append(word)
+    	else:
+    		word_length_dict[len(word)] = [word]
+    for k, v in word_length_dict.iteritems():
+    	print k, v
+
+    print list_of_lengths
+
 
 word_length(words)
 
